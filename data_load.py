8ba6d637cd7c18b1336af69f876c4787c1a5d07e
gabir-yusuf <gabir.yusuf@gmail.com>
2020-02-01 22:15:28 +0200

    code reformat

--- data_load.py
+++ data_load.py
@@ -1,4 +1,3 @@
-import glob
 import os
 import torch
 from torch.utils.data import Dataset, DataLoader
@@ -27,13 +26,11 @@ class FacialKeypointsDataset(Dataset):
         return len(self.key_pts_frame)
 
     def __getitem__(self, idx):
-        image_name = os.path.join(self.root_dir,
-                                  self.key_pts_frame.iloc[idx, 0])
-
+        image_name = os.path.join(self.root_dir, self.key_pts_frame.iloc[idx, 0])
         image = mpimg.imread(image_name)
 
         # if image has an alpha color channel, get rid of it
-        if(image.shape[2] == 4):
+        if image.shape[2] == 4:
             image = image[:, :, 0:3]
 
         key_pts = self.key_pts_frame.iloc[idx, 1:].as_matrix()
@@ -46,8 +43,6 @@ class FacialKeypointsDataset(Dataset):
         return sample
 
 
-# tranforms
-
 class Normalize(object):
     """Convert a color image to grayscale and normalize the color range to [0,1]."""
 
@@ -58,14 +53,14 @@ class Normalize(object):
         key_pts_copy = np.copy(key_pts)
 
         # convert image to grayscale
-        image_copy = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+        image_copy = cv2.cvtColor(image_copy, cv2.COLOR_RGB2GRAY)
 
         # scale color range from [0, 255] to [0, 1]
         image_copy = image_copy / 255.0
 
         # scale keypoints to be centered around 0 with a range of [-1, 1]
         # mean = 100, sqrt = 50, so, pts should be (pts - 100)/50
-#         key_pts_copy = (key_pts_copy - 100)/50.0
+        # key_pts_copy = (key_pts_copy - 100)/50.0
         # NaimishNet paper p3 Data Pre-processing
         key_pts_copy = (key_pts_copy - 100) / 50.0
 
@@ -86,8 +81,8 @@ class Rescale(object):
 
     def __call__(self, sample):
         image, key_pts = sample['image'], sample['keypoints']
-
         h, w = image.shape[:2]
+
         if isinstance(self.output_size, int):
             if h > w:
                 new_h, new_w = self.output_size * h / w, self.output_size
@@ -97,7 +92,6 @@ class Rescale(object):
             new_h, new_w = self.output_size
 
         new_h, new_w = int(new_h), int(new_w)
-
         img = cv2.resize(image, (new_w, new_h))
 
         # scale the pts, too
@@ -139,13 +133,13 @@ class RandomCrop(object):
 
 
 class ToTensor(object):
-    """Convert ndarrays in sample to Tensors."""
+    """Convert numpy arrays in sample to Tensors."""
 
     def __call__(self, sample):
         image, key_pts = sample['image'], sample['keypoints']
 
         # if image has no grayscale color channel, add one
-        if(len(image.shape) == 2):
+        if len(image.shape) == 2:
             # add that third color dim
             image = image.reshape(image.shape[0], image.shape[1], 1)
 
